eb10bded1350c8ccab8cde5ffccb9a414b605568
Daniil Frangov <frangov7@gmail.com>
2020-08-30 13:41:00 +0300

    deploy

---
+++ precache-manifest.62fe8e404f8de52f5433b0ad03e3dea0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a800ca3c06ed29fc39ec",
+    "url": "/css/app.9c5a928c.css"
+  },
+  {
+    "revision": "2850c3784f0437e1dc7d",
+    "url": "/css/chunk-vendors.24bf1742.css"
+  },
+  {
+    "revision": "f8366d750f0df222ced6483dabb0715e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a800ca3c06ed29fc39ec",
+    "url": "/js/app.b8330f5b.js"
+  },
+  {
+    "revision": "2850c3784f0437e1dc7d",
+    "url": "/js/chunk-vendors.08338da3.js"
+  },
+  {
+    "revision": "af32d0de4ac9f6c5983e9beafb3e4db2",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/robots.txt"
+  }
+]);
